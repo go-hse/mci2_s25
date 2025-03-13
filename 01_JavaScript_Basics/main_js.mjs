8409cf1e7387466be7738ba3b779a2f731deb22e
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2025-03-13 07:44:51 +0100

    initial setup

---
+++ 01_JavaScript_Basics/main_js.mjs
@@ -0,0 +1,11 @@
+// import * as lib from "./js/funcs.mjs";
+
+import { test } from "./js/funcs.mjs"
+
+
+window.onload = () => {
+    // lib.test();
+    test();
+
+};
+
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Studiengang Softwaretechnik und Mendieninformatik
 
+
+
