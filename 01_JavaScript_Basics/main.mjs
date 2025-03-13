3398af9da9f27771447f4c7f0bd5a4cdcf7ddf95
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2025-03-13 12:11:11 +0100

    animierte Rechtecke

--- 01_JavaScript_Basics/main.mjs
+++ 01_JavaScript_Basics/main.mjs
@@ -1,10 +1,13 @@
 // import * as lib from "./js/funcs.mjs";
 
-import { test } from "./js/funcs.mjs"
+import { test, Hello } from "./js/funcs.mjs"
 
 window.onload = () => {
     // lib.test();
     test();
+
+    // lib.Hello();
+    Hello();
 
 };
 
---
+++ 02_Canvas_Beispiele/main.mjs
@@ -0,0 +1,52 @@
+// import * as lib from "./js/funcs.mjs";
+
+// import { test, Hello } from "./js/funcs.mjs"
+
+
+window.onload = () => {
+    let cnv = document.getElementById("cnv");
+    let ctx = cnv.getContext("2d");
+
+    function resize() {
+        cnv.width = window.innerWidth;
+        cnv.height = window.innerHeight;
+    }
+    addEventListener("resize", resize);
+
+    let alpha = 0;
+    const rect_width = 200;
+    const rect_height = 20;
+
+    function draw() {
+        ctx.resetTransform();
+        ctx.clearRect(0, 0, cnv.width, cnv.height);
+        ctx.fillStyle = "#f00";
+        ctx.translate(200, 300);
+
+        alpha += 0.01;
+        ctx.rotate(alpha);
+        ctx.fillRect(0, 0, rect_width, rect_height);
+
+        ctx.translate(0, 50);
+        ctx.fillStyle = "#0f0";
+        ctx.fillRect(0, 0, rect_width, rect_height);
+
+        ctx.resetTransform();
+        ctx.translate(cnv.width / 2, cnv.height / 2);
+        ctx.fillStyle = "#00f";
+        ctx.translate(0, 50);
+        ctx.rotate(alpha);
+        ctx.translate(-rect_width / 2, -rect_height / 2);
+        ctx.fillRect(0, 0, rect_width, rect_height);
+
+        window.requestAnimationFrame(draw);
+    }
+    resize();
+    draw();
+}
+
+
+// window.onload = Init
+// addEventListener("load", Init);
+
+
