2f1648f49b81134ace6b5dbcd2de0a9e184094cd
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2025-03-27 13:03:06 +0100

    Greifen

--- 08_Touch_Vis/main.mjs
+++ 08_Touch_Vis/main.mjs
@@ -1,28 +1,4 @@
-function polygon(ctx, x, y, noOfCorners, radius, fillStyle = "#aaa", strokeStyle = "#000", lineWidth = "3", txt = "") {
-    ctx.save();
-    const angle = 2 * Math.PI / noOfCorners;
-
-    ctx.translate(x, y);
-    ctx.beginPath();
-    ctx.moveTo(radius, 0);
-
-    for (let i = 0; i < noOfCorners; ++i) {
-        ctx.rotate(angle);
-        ctx.lineTo(radius, 0);
-    }
-
-    ctx.fillStyle = fillStyle;
-    ctx.strokeStyle = strokeStyle;
-    ctx.lineWidth = lineWidth;
-    ctx.stroke();
-    ctx.fill();
-
-    ctx.fillStyle = "#000";
-    ctx.fillText(txt, 0, 0);
-
-    ctx.restore();
-}
-
+import { createInteractivePath, createUpath } from "./js/function.mjs";
 
 window.onload = () => {
     const cnv = document.getElementById("cnv");
@@ -34,34 +10,29 @@ window.onload = () => {
     }
     addEventListener("resize", resize);
 
+    const iU = createInteractivePath(ctx, 100, 100, createUpath(), "#f00");
+
     const radius = 50;
     const distance = radius * 2.5;
 
-    const Touches = {};
     cnv.addEventListener("touchstart", (ev) => {
         ev.preventDefault();
         for (let t of ev.changedTouches) {
-            Touches[t.identifier] = {
-                x: t.pageX,
-                y: t.pageY
-            }
+            iU.isTouched(t.identifier, t.pageX, t.pageY);
         }
     });
 
     cnv.addEventListener("touchend", (ev) => {
         ev.preventDefault();
         for (let t of ev.changedTouches) {
-            delete Touches[t.identifier];
+            iU.reset(t.identifier);
         }
     });
 
     cnv.addEventListener("touchmove", (ev) => {
         ev.preventDefault();
         for (let t of ev.changedTouches) {
-            Touches[t.identifier] = {
-                x: t.pageX,
-                y: t.pageY
-            }
+            iU.move(t.identifier, t.pageX, t.pageY);
         }
     });
 
@@ -70,11 +41,7 @@ window.onload = () => {
         ctx.resetTransform();
         ctx.clearRect(0, 0, cnv.width, cnv.height);
 
-        const identifiers = Object.keys(Touches);
-        for (let identifier of identifiers) {
-            const touch = Touches[identifier];
-            polygon(ctx, touch.x, touch.y, 3, 30);
-        }
+        iU.draw(ctx);
 
         window.requestAnimationFrame(draw);
     }
