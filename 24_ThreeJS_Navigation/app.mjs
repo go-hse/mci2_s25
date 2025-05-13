e8a90d30844628204d54b7e63b707c9a2654b9d1
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2025-05-13 16:20:03 +0200

    Adding Navigation

---
+++ 24_ThreeJS_Navigation/app.mjs
@@ -0,0 +1,276 @@
+import * as THREE from '../99_Lib/three.module.min.js';
+import { keyboard, mouse } from './js/interaction2D.mjs';
+import { add, createLine, loadGLTFcb, randomMaterial, shaderMaterial } from './js/geometry.mjs';
+import { createRay } from './js/ray.mjs';
+
+
+import { EffectComposer } from '../99_Lib/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from '../99_Lib/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from '../99_Lib/jsm/postprocessing/UnrealBloomPass.js';
+import { OutputPass } from '../99_Lib/jsm/postprocessing/OutputPass.js';
+
+
+import { VRButton } from '../99_Lib/jsm/webxr/VRButton.js';
+import { createVRcontrollers } from './js/vr.mjs';
+
+window.onload = async function () {
+    const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 100);
+    camera.position.set(0, 0.3, 2);
+
+    const scene = new THREE.Scene();
+    const world = new THREE.Group();
+    world.matrixAutoUpdate = false;
+    scene.add(world);
+
+    scene.background = new THREE.Color(0x666666);
+
+    const hemiLight = new THREE.HemisphereLight();
+    scene.add(hemiLight);
+
+    const dirLight = new THREE.DirectionalLight(0xffffff, 3);
+    dirLight.position.set(5, 5, 5);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.zoom = 2;
+    scene.add(dirLight);
+
+
+    //////////////////////////////////////////////////////////////////////////////
+    // FLOOR
+    // const floorMaterial = await shaderMaterial("./shaders/floorVertexShader.glsl", "./shaders/floorFragmentShader.glsl")
+
+
+    const width = 0.1;
+    const box = new THREE.BoxGeometry(10, width, 10, 10, 1, 10);
+    const floor = new THREE.Mesh(box, randomMaterial());
+    floor.position.y = -1;
+    floor.receiveShadow = true;
+    floor.userData.physics = { mass: 0 };
+    floor.name = "floor";
+
+    const wireframe = new THREE.WireframeGeometry(box);
+    const line = new THREE.LineSegments(wireframe);
+    line.material.opacity = 0.25;
+    line.material.transparent = true;
+    line.position.y = floor.position.y;
+    scene.add(line);
+
+    scene.add(floor);
+
+
+    const cursor = add(1, scene);
+    const isMouseButton = mouse(cursor);
+
+    let objects = [];
+    let x = -0.8, y = 0.3, z = -0.5, delta = 0.4;
+    for (let i = 0; i < 5; ++i) {
+        objects.push(add(i, world, x, y, z)); x += delta;
+    }
+
+    loadGLTFcb('./models/cube_with_inner_sphere.glb', (gltf) => {
+        gltf.scene.traverse(child => {
+            if (child.name.includes("geo")) {
+                objects.push(child);
+                child.scale.set(0.2, 0.2, 0.2) // scale here
+                child.position.set(1, 0.5, 0);
+                child.updateMatrix();
+                child.matrixAutoUpdate = false;
+            }
+        });
+        world.add(gltf.scene);
+    });
+
+
+    const lineFunc = createLine(scene);
+    const rayFunc = createRay(objects);
+
+    let position = new THREE.Vector3();
+    let rotation = new THREE.Quaternion();
+    let scale = new THREE.Vector3();
+    let endRay = new THREE.Vector3();
+    let direction = new THREE.Vector3();
+
+    // Renderer erstellen
+    const renderer = new THREE.WebGLRenderer({
+        antialias: true,
+    });
+
+    // Renderer-Parameter setzen
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.toneMapping = THREE.ReinhardToneMapping;
+    renderer.toneMappingExposure = Math.pow(1, 4.0);
+    renderer.shadowMap.enabled = true;
+    document.body.appendChild(renderer.domElement);
+    document.body.appendChild(VRButton.createButton(renderer));
+
+    //
+    let last_active_controller;
+    createVRcontrollers(scene, renderer, (controller, data, id) => {
+        cursor.matrixAutoUpdate = false;
+        cursor.visible = false;
+        last_active_controller = controller;
+        renderer.xr.enabled = true;
+        console.log("verbinde", id, data.handedness)
+    });
+
+    const params = {
+        threshold: 0,
+        strength: 1,
+        radius: 0,
+        exposure: 1
+    };
+
+    const renderScene = new RenderPass(scene, camera);
+
+    const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85);
+    bloomPass.threshold = params.threshold;
+    bloomPass.strength = params.strength;
+    bloomPass.radius = params.radius;
+
+    const outputPass = new OutputPass();
+
+    const composer = new EffectComposer(renderer);
+    composer.addPass(renderScene);
+    composer.addPass(bloomPass);
+    composer.addPass(outputPass);
+
+
+    window.addEventListener('resize', onWindowResize);
+    function onWindowResize() {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+        renderer.setSize(width, height);
+        composer.setSize(width, height);
+    }
+
+
+    const addKey = keyboard();
+    addKey("Escape", active => {
+        console.log("Escape", active);
+    });
+
+    let grabbed = false, squeezed = false;
+    addKey(" ", active => {
+        console.log("Space: Grabbed", active);
+        grabbed = active;
+    });
+
+    addKey("s", active => {
+        console.log("S: Squeeze", active);
+        squeezed = active;
+    });
+
+    addKey("f", active => {
+        if (active) {
+            console.log("F: toggle floor", active, floor.visible);
+            floor.visible = !floor.visible;
+        }
+    });
+
+    addKey("r", active => {
+        console.log("R: reset world", active, floor.visible);
+        world.matrix.identity();
+    });
+
+
+    const maxDistance = 10;
+    direction.set(0, 1, 0);
+
+    let grabbedObject, initialGrabbed, distance, inverseHand, inverseWorld;
+    const deltaFlyRotation = new THREE.Quaternion();
+    const differenceMatrix = new THREE.Matrix4();
+    const flySpeedRotationFactor = 0.01;
+    const flySpeedTranslationFactor = -0.02;
+    const euler = new THREE.Euler();
+
+
+    // Renderer-Loop starten
+    function render() {
+
+        if (last_active_controller) {
+            cursor.matrix.copy(last_active_controller.matrix);
+            squeezed = last_active_controller.userData.isSqueezeing;
+            grabbed = last_active_controller.userData.isSelecting;
+            direction.set(0, 0, -1);
+        } else {
+            direction.set(0, 1, 0);
+        }
+
+        cursor.matrix.decompose(position, rotation, scale);
+        lineFunc(0, position);
+
+        direction.applyQuaternion(rotation);
+
+        let firstObjectHitByRay;
+        if (grabbedObject === undefined) {
+            firstObjectHitByRay = rayFunc(position, direction);
+            if (firstObjectHitByRay) {
+                console.log(firstObjectHitByRay.object.name, firstObjectHitByRay.distance);
+                distance = firstObjectHitByRay.distance;
+            } else {
+                distance = maxDistance;
+            }
+            endRay.addVectors(position, direction.multiplyScalar(distance));
+            lineFunc(1, endRay);
+        }
+
+
+        if (grabbed) {
+            if (grabbedObject) {
+                endRay.addVectors(position, direction.multiplyScalar(distance));
+                lineFunc(1, endRay);
+                if (grabbedObject === world) {
+                    world.matrix.copy(cursor.matrix.clone().multiply(initialGrabbed));
+                } else {
+                    grabbedObject.matrix.copy(inverseWorld.clone().multiply(cursor.matrix).multiply(initialGrabbed));
+                }
+            } else if (firstObjectHitByRay) {
+                grabbedObject = firstObjectHitByRay.object;
+                inverseWorld = world.matrix.clone().invert();
+                initialGrabbed = cursor.matrix.clone().invert().multiply(world.matrix).multiply(grabbedObject.matrix);
+            } else {
+                grabbedObject = world;
+                initialGrabbed = cursor.matrix.clone().invert().multiply(world.matrix);
+            }
+        } else {
+            grabbedObject = undefined;
+        }
+
+        if (squeezed) {
+            lineFunc(1, position);
+            if (inverseHand !== undefined) {
+                let differenceHand = cursor.matrix.clone().multiply(inverseHand);
+                differenceHand.decompose(position, rotation, scale);
+                deltaFlyRotation.set(0, 0, 0, 1);
+                deltaFlyRotation.slerp(rotation.conjugate(), flySpeedRotationFactor);
+
+                // Beschränkung der Rotation beim Fliegen
+                euler.setFromQuaternion(deltaFlyRotation);
+                euler.x = 0;
+                euler.z = 0;
+                deltaFlyRotation.setFromEuler(euler);
+
+                differenceMatrix.compose(position.multiplyScalar(flySpeedTranslationFactor), deltaFlyRotation, scale);
+                world.matrix.premultiply(differenceMatrix);
+            } else {
+                inverseHand = cursor.matrix.clone().invert();
+            }
+        } else {
+            inverseHand = undefined;
+        }
+        renderer.render(scene, camera);
+        // composer.render();
+
+    }
+    renderer.setAnimationLoop(render);
+};
+
+
+/*
+- Laden von Objekten
+- 
+
+
+*/
